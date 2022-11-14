279c561e435169273e9f6b338ffd89df6cca110f
Mayank Gupta <techfreakworm@gmail.com>
2022-11-15 01:58:16 +0530

    Use Tailrec for tail recursive functions

--- src/lectures/part1basics/Recursion.scala
+++ src/lectures/part1basics/Recursion.scala
@@ -1,5 +1,6 @@
 package lectures.part1basics
 
+import scala.annotation.tailrec
 import scala.jdk.Accumulator
 
 object Recursion extends App {
@@ -19,6 +20,7 @@ object Recursion extends App {
 //  println(factorial(5000))
 
   def anotherFactorial(n: Int): BigInt = {
+    @tailrec
     def factHelper(x: Int, accumulator: BigInt): BigInt = {
       if (x<=1) accumulator
       else factHelper(x - 1, x * accumulator)
@@ -41,7 +43,49 @@ object Recursion extends App {
   */
 
 
-  println(anotherFactorial(50000))
+  println(anotherFactorial(20000))
+
+  // WHEN YOU NEED LOOPS, USE TAIL RECURSION
+
+  /*
+  1. Concatenate a string n times
+  2. IsPrime function that is tail recursive
+  3. Fibonacci function that is tail recursive
+   */
+
+  @tailrec
+  def concatenateTailrec(aString: String, n: Int, accumulator: String): String = {
+    if (n <= 0) accumulator
+    else concatenateTailrec(aString, n-1, aString + accumulator)
+  }
+
+  println(concatenateTailrec("Hello", 3, ""))
+
+
+  def isPrime(n: Int): Boolean = {
+    @tailrec
+    def isPrimeTailrec(t: Int, isStillPrime: Boolean): Boolean = {
+      if (!isStillPrime) true
+      else if (t<=1) true
+      else isPrimeTailrec(t - 1, n % t != 0 && isStillPrime)
+    }
+
+    isPrimeTailrec(n / 2, true)
+  }
+
+  println(isPrime(2003))
+
+  def fibonacci(n: Int) = {
+    def fiboTailrec(i: Int, last: Int, nextToLast: Int): Int = {
+      if (i >= n) last
+      else fiboTailrec(i + 1, last + nextToLast, last)
+    }
+
+    if (n<=2) 1
+    else fiboTailrec(2, 1, 1)
+  }
+
+  println(fibonacci(8))
 
 
 }
