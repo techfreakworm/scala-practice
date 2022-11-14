cfc23948dc60bd1e2ed36812adc6aaf5538b6503
Mayank Gupta <techfreakworm@gmail.com>
2022-11-15 01:48:01 +0530

    Accumulator approach to optimise recursion

---
+++ src/lectures/part1basics/Recursion.scala
@@ -0,0 +1,47 @@
+package lectures.part1basics
+
+import scala.jdk.Accumulator
+
+object Recursion extends App {
+
+  def factorial(n: Int): BigInt = {
+    if (n <=1 ) 1
+    else {
+      println("Computing factorial of " + n + " - I first need factorial of " +  (n-1))
+      val result = n*factorial(n-1)
+      println("Computed factorial of " + n)
+      result
+    }
+
+  }
+
+//  println(factorial(10))
+//  println(factorial(5000))
+
+  def anotherFactorial(n: Int): BigInt = {
+    def factHelper(x: Int, accumulator: BigInt): BigInt = {
+      if (x<=1) accumulator
+      else factHelper(x - 1, x * accumulator)
+    }
+
+    factHelper(n, 1)
+  }
+
+
+
+  /*
+  *anotherFactorial(10) = factHelper(10, 1)
+  * = factHelper(9, 10*1)
+  * = factHelper(8, 9*10*1)
+  * = factHelper(7, 8*9*10*1)
+  * ....
+  * = factHelper(2, 3*4*.....10*1)
+  * = factHelper(1, 2*3*4.......10*1)
+  *
+  */
+
+
+  println(anotherFactorial(50000))
+
+
+}
