a5bfa67f43e93a6dc3ef3ee78ae5b8393adfe403
Mayank Gupta <techfreakworm@gmail.com>
2022-11-16 10:49:59 +0530

    Add exercise question

--- src/lectures/part2oop/AnonymousClasses.scala
+++ src/lectures/part2oop/AnonymousClasses.scala
@@ -30,4 +30,21 @@ object AnonymousClasses extends App{
     override def sayHi: Unit = println(s"Hi, my name is Jim, how can I help?")
   }
 
+  /*
+    1. Generic trait MyPredicate[-T] with a little method test(T) ==>Boolean
+    2. Generic trait MyTransformer[-A,B] with a method transform(A) => B
+    3. MyList:
+      - map(transformer) => MyList
+      - filter(predicate) => MyList
+      - flatMap(transformer from A to MyList[B]) => MyList[b
+
+      class EvenPredicate extends MyPredicate[Int]
+      class StringToIntTransformer extends MyTransformer[String, Int]
+
+      [1,2,3].map(n*2) = [2,4,6]
+      [1,2,3,4].filter(n%2) == [2,4]
+      [1,2,3].flatMap(n => [n,n+1]) => [1,2,2,3,3,4]
+
+   */
+
 }
