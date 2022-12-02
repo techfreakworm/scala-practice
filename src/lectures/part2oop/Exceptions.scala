74f7106a5af034cd4c1f54c8ff6a6f50b350abff
Mayank Gupta <techfreakworm@gmail.com>
2022-12-03 01:10:49 +0530

    Scala exceptions done

--- src/lectures/part2oop/Exceptions.scala
+++ src/lectures/part2oop/Exceptions.scala
@@ -39,7 +39,7 @@ object Exceptions extends App {
   class MyException extends Exception
   val exception = new MyException
 
-  throw exception
+//  throw exception
 
   /*
     1. Crash your program with OutOfMemoryError
@@ -56,4 +56,49 @@ object Exceptions extends App {
       - MathCalculationException for division by 0
 
   */
+// OOM
+//  val array = Array.ofDim[Int](Int.MaxValue)
+
+  // SO
+//  def infinite: Int = 1 + infinite
+//  val noLimit = infinite
+
+  class OverflowException extends RuntimeException
+  class UnderflowException extends RuntimeException
+  class MathCalculationException extends RuntimeException("Division by 0")
+
+  object PocketCalculator {
+    def add(x: Int, y: Int) = {
+      val result = x + y
+      if (x > 0 && y > 0 && result < 0) throw new OverflowException
+      else if (x < 0 && y < 0 && result > 0) throw new UnderflowException
+      else result
+    }
+
+    def subtract(x: Int, y: Int) = {
+      val result = x - y
+      if (x > 0 && y < 0 && result < 0) throw new OverflowException
+      else if (x < 0 && y > 0 && result > 0) throw new UnderflowException
+      else result
+    }
+
+    def multiply(x: Int, y: Int) = {
+      val result = x*y
+      if(x>0 && y>0 && result <0) throw new OverflowException
+      else if(x<0 && y<0 && result <0) throw new OverflowException
+      else if(x>0 && y<0 && result >0) throw new UnderflowException
+      else if(x<0 && y<0 && result >0) throw new UnderflowException
+      else result
+    }
+
+    def divide(x: Int, y: Int) = {
+      if (y == 0) throw new MathCalculationException
+      else x / y
+    }
+
+  }
+
+//  println(PocketCalculator.add(Int.MaxValue, 2))
+  println(PocketCalculator.divide(2,0))
+
 }
